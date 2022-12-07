b0cc3153948553a3d53fdeacfe032dd6371a637a
Wojciech Górka <wojowany@gmail.com>
2022-12-07 22:03:59 +0100

    js localstorage script

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,3 +1,15 @@
 import '../scss/editor.scss';
 
 const textarea = document.querySelector('.main__textarea--js');
+const load = document.querySelector('.button__load--js');
+const save = document.querySelector('.button__save--js');
+
+const storage = "";
+
+save.addEventListener('click', () => {
+    localStorage.setItem('textarea', textarea.value);
+})
+
+load.addEventListener('click', () => {
+    textarea.value = localStorage.getItem('textarea');
+})
